749834de6b5273e84ac4eed4238f79c5dc615935
Jiwon <kimg1623@naver.com>
2024-03-15 10:06:06 +0900

    chore: 코드 정리(컨트롤러, 서비스)

--- src/main/java/com/sherpa/exambank/method/controller/MethodController.java
+++ src/main/java/com/sherpa/exambank/method/controller/MethodController.java
@@ -35,79 +35,14 @@ public class MethodController {
     @PostMapping("/step0")
     public String postStep0Page(@RequestParam("subjectId") String subjectId, Model model) throws InstantiationException, IllegalAccessException {
 
-        // 교과서 정보 얻어오기
-        Step0ChapterResponse step0ChapterResponse =  methodService.postForChapterListBySubjectId(subjectId);
-        String subjectName = step0ChapterResponse.getChapterList().get(0).getSubjectName();
-        String curriculumName = step0ChapterResponse.getChapterList().get(0).getCurriculumName();
+        List<String> subjectInfo = methodService.findSubjectNameBySubjectId(subjectId);
+        model.addAttribute("subjectId", subjectId);
+        model.addAttribute("subjectName", subjectInfo.get(0));
+        model.addAttribute("curriculumName", subjectInfo.get(1));
 
-        // 교과서
-        Step0ExamResponse step0ExamResponse =  methodService.postForExamListBySubjectId(subjectId);
-        List<ExamList> examList = step0ExamResponse.getExamList();
+        ArrayList<ExamGroupByLargeChapterResponse> examListResponse = methodService.findSettingListBySubjectId(subjectId);
+        model.addAttribute("examListResponse",examListResponse);
 
-        HashMap<String,String> largeChapterMap = new HashMap<>(); // largeChapterId, largeChapterName
-
-        ArrayList<String> largeChapterIdList = new ArrayList<>();
-        ArrayList<String> largeChapterNameList = new ArrayList<>();
-        ArrayList<ArrayList<String>> examListByLargeChapterId = new ArrayList<>();
-
-        for (ExamList exam : examList) {
-
-            // 대단원 id 추출 후 리스트 추가
-            String largeChapterId = exam.getLargeChapterId();
-            if (!largeChapterIdList.contains(largeChapterId)) {
-                largeChapterIdList.add(largeChapterId);
-            }
-
-            // 대단원 name 추출 후 리스트 추가
-            String largeChapterName = exam.getLargeChapterName();
-            if (!largeChapterNameList.contains(largeChapterName))
-                largeChapterNameList.add(largeChapterName);
-
-            // 대단원 Id 포함 시험지 정보 추출 후 리스트 추가
-            ArrayList<String> examInfo = new ArrayList<>();
-            examInfo.add(largeChapterId);
-            examInfo.add(exam.getExamId());
-            examInfo.add(exam.getExamName());
-            examInfo.add(String.valueOf(exam.getItemCnt()));
-            examListByLargeChapterId.add(examInfo);
-        }
-        largeChapterIdList.sort(String::compareTo);
-        log.info(largeChapterIdList.toString());
-        largeChapterNameList.sort(String::compareTo);
-        log.info(largeChapterNameList.toString());
-
-
-        ArrayList<ExamGroupByLargeChapterResponse> examGroupByLargeChapterResponses = new ArrayList<>();
-        for(String id : largeChapterIdList){
-            examGroupByLargeChapterResponses.add(
-                    new ExamGroupByLargeChapterResponse(id, largeChapterNameList.get(largeChapterIdList.indexOf(id)))
-            );
-            log.info(examGroupByLargeChapterResponses.toString());
-        }
-
-        for (ArrayList<String> examInfo: examListByLargeChapterId) {
-//            if(String.valueOf(examInfo.get(0)).equals())
-            log.info(String.valueOf(examInfo.get(0)));
-            for(String id:largeChapterIdList) {
-                if (String.valueOf(examInfo.get(0)).equals(id)){
-                    ExamGroupByLargeChapterResponse matchedResponse = examGroupByLargeChapterResponses.get(largeChapterIdList.indexOf(id));
-                    matchedResponse.getExamInfoList().add(new ExamGroupByLargeChapterResponse.ExamInfo(
-                            examInfo.get(1),examInfo.get(2),examInfo.get(3)
-                    ));
-                }
-            }
-        }
-        log.info(String.valueOf(examGroupByLargeChapterResponses));
-        model.addAttribute("response",examGroupByLargeChapterResponses);
-
-
-        
-        
-        
-        
-        
-        model.addAttribute("chapterList", step0ChapterResponse.getChapterList());
-        model.addAttribute("examList", step0ExamResponse.getExamList());
 
         return "customexam/step0";
     }
--- src/main/java/com/sherpa/exambank/method/service/MethodService.java
+++ src/main/java/com/sherpa/exambank/method/service/MethodService.java
@@ -1,5 +1,7 @@
 package com.sherpa.exambank.method.service;
 
+import com.sherpa.exambank.method.domain.ExamGroupByLargeChapterResponse;
+import com.sherpa.exambank.method.domain.ExamList;
 import com.sherpa.exambank.method.domain.Step0ChapterResponse;
 import com.sherpa.exambank.method.domain.Step0ExamResponse;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,9 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -24,9 +29,91 @@ public class MethodService {
     @Value("${tsherpa.api.url}")
     private String tsherpaURL;
 
-    /*public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
-    }*/
+    /**
+     * step0 상단 교과서명과 교육과정 정보 출력을 위한 조회
+     * @param subjectId 교과서ID
+     * @return 리스트[교과서명, 교육과정]
+     */
+    public List<String> findSubjectNameBySubjectId(String subjectId) throws InstantiationException, IllegalAccessException {
+        // 교과서 정보 얻어오기
+        Step0ChapterResponse step0ChapterResponse = postForChapterListBySubjectId(subjectId); // api #1 교재별 단원 리스트 조회
+
+        String subjectName = step0ChapterResponse.getChapterList().get(0).getSubjectName();
+        String curriculumName = step0ChapterResponse.getChapterList().get(0).getCurriculumName();
+
+        List<String> result = new ArrayList<>();
+        result.add(subjectName);    // [0]
+        result.add(curriculumName); // [1]
+
+        return result;
+    }
+
+    /**
+     * step0 대단원명, 대단원별 셋팅지 정보 조회
+     * @param subjectId 교과서ID
+     * @return 대단원을 기준으로 그룹화된 셋팅지 응답 리스트
+     */
+    public ArrayList<ExamGroupByLargeChapterResponse> findSettingListBySubjectId(String subjectId) {
+        // 셋팅지 리스트 얻어오기
+        Step0ExamResponse step0ExamResponse =  postForExamListBySubjectId(subjectId); // api #16 교재&대단원 별 T셀파 셋팅지 리스트 조회
+        List<ExamList> examList = step0ExamResponse.getExamList(); // 셋팅지 리스트만 List에 담기
+
+        // 대단원 기준 셋팅지 그룹화 작업
+        ArrayList<String> largeChapterIdList = new ArrayList<>();
+        ArrayList<String> largeChapterNameList = new ArrayList<>();
+        ArrayList<ArrayList<String>> examListByLargeChapterId = new ArrayList<>();
+
+        for (ExamList exam : examList) {
+
+            // 대단원 id 추출 후 리스트 추가
+            String largeChapterId = exam.getLargeChapterId();
+            if (!largeChapterIdList.contains(largeChapterId)) {
+                largeChapterIdList.add(largeChapterId);
+            }
+
+            // 대단원 name 추출 후 리스트 추가
+            String largeChapterName = exam.getLargeChapterName();
+            if (!largeChapterNameList.contains(largeChapterName))
+                largeChapterNameList.add(largeChapterName);
+
+            // 대단원 Id 포함 시험지 정보 추출 후 리스트 추가
+            ArrayList<String> examInfo = new ArrayList<>();
+            examInfo.add(largeChapterId);
+            examInfo.add(exam.getExamId());
+            examInfo.add(exam.getExamName());
+            examInfo.add(String.valueOf(exam.getItemCnt()));
+            examListByLargeChapterId.add(examInfo);
+        }
+        // 대단원 ID, 대단원 Name 정렬
+        largeChapterIdList.sort(String::compareTo);
+        largeChapterNameList.sort(String::compareTo);
+
+
+        ArrayList<ExamGroupByLargeChapterResponse> examGroupByLargeChapterResponses = new ArrayList<>();
+        for(String id : largeChapterIdList){
+            examGroupByLargeChapterResponses.add(
+                    new ExamGroupByLargeChapterResponse(id, largeChapterNameList.get(largeChapterIdList.indexOf(id)))
+            );
+            log.info(examGroupByLargeChapterResponses.toString());
+        }
+
+        for (ArrayList<String> examInfo: examListByLargeChapterId) {
+//            if(String.valueOf(examInfo.get(0)).equals())
+            log.info(String.valueOf(examInfo.get(0)));
+            for(String id:largeChapterIdList) {
+                if (String.valueOf(examInfo.get(0)).equals(id)){
+                    ExamGroupByLargeChapterResponse matchedResponse = examGroupByLargeChapterResponses.get(largeChapterIdList.indexOf(id));
+                    matchedResponse.getExamInfoList().add(new ExamGroupByLargeChapterResponse.ExamInfo(
+                            examInfo.get(1),examInfo.get(2),examInfo.get(3)
+                    ));
+                }
+            }
+        }
+        log.info(String.valueOf(examGroupByLargeChapterResponses));
+        return examGroupByLargeChapterResponses;
+
+
+    }
 
     public Step0ChapterResponse postForChapterListBySubjectId(String subjectId) throws InstantiationException, IllegalAccessException {
         // 요청 url
