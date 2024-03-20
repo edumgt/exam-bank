08a307d99a5a6c16740dff28a0d4d4a2a131b306
thegreatjy <thegreatjy@gmail.com>
2024-03-20 10:25:11 +0900

    refactor: lombok 사용 및 접근 제한자 defeault로 변경

--- src/main/java/com/sherpa/exambank/step1/domain/MinorClassification.java
+++ src/main/java/com/sherpa/exambank/step1/domain/MinorClassification.java
@@ -1,65 +1,23 @@
 package com.sherpa.exambank.step1.domain;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Objects;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MinorClassification {
-    private String subject;
-    private String large;
-    private String medium;
-    private String small;
-    private String topic;
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getLarge() {
-        return large;
-    }
-
-    public void setLarge(String large) {
-        this.large = large;
-    }
-
-    public String getMedium() {
-        return medium;
-    }
-
-    public void setMedium(String medium) {
-        this.medium = medium;
-    }
-
-    public String getSmall() {
-        return small;
-    }
-
-    public void setSmall(String small) {
-        this.small = small;
-    }
-
-    public String getTopic() {
-        return topic;
-    }
-
-    public void setTopic(String topic) {
-        this.topic = topic;
-    }
-
-    public MinorClassification() {
-    }
-
-    public MinorClassification(String subject, String large, String medium, String small, String topic) {
-        this.subject = subject;
-        this.large = large;
-        this.medium = medium;
-        this.small = small;
-        this.topic = topic;
-    }
+    String subject;
+    Long large;
+    Long medium;
+    Long small;
+    Long topic;
 
     @Override
     public String toString() {
@@ -71,18 +29,5 @@ public class MinorClassification {
                 ", \"small\":\"" + small +  "\"" +
                 ", \"topic\":\"" + topic +  "\"" +
                 "}";
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MinorClassification that = (MinorClassification) o;
-        return Objects.equals(subject, that.subject) && Objects.equals(large, that.large) && Objects.equals(medium, that.medium) && Objects.equals(small, that.small) && Objects.equals(topic, that.topic);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(subject, large, medium, small, topic);
     }
 }
