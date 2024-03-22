85397870dddec1cc068d92c2b3ed87a8b0e131fd
thegreatjy <thegreatjy@gmail.com>
2024-03-22 11:30:23 +0900

    feat: 출제옵션 중 평가영역, 문제형태, 난이도 구성 버튼 동작 기능 구현

--- src/main/webapp/resource/popup/js/stepOne.js
+++ src/main/webapp/resource/popup/js/stepOne.js
@@ -1,5 +1,7 @@
 let tempLevelArray;
-let active=false;
+let active= false;
+
+/* 자동실행 함수 */
 
 $(function () {
 
@@ -280,7 +282,48 @@ $(function () {
         }
     });
 
+    // 출제옵션 > 평가영역
+    $('#category-btn-group button').on("click", function () {
+        let btnId = $.trim(String($(this).attr("id")));
+
+        if($("#" + btnId).hasClass("active") === true) { // 활성화 되어 있다.
+            $("#" + btnId).removeClass("active");
+        } else { // 비활성화 되어 있다.
+            $("#" + btnId).addClass("active");
+        }
+    });
+
+    // 출제옵션 > 문제 형태
+    $('#type-btn-group button').on("click", function () {
+        let typeBtnId = $.trim(String($(this).attr("id")));
+
+        if($("#" + typeBtnId).hasClass("active") === true) { // 활성화 되어 있다.
+            $("#" + typeBtnId).removeClass("active");
+        } else { // 비활성화 되어 있다.
+            $("#" + typeBtnId).addClass("active");
+        }
+    });
+
+    // 출제옵션 > 난이도 구성
+    $('#level-btn-group button').on("click", function () {
+        let dataStep = $.trim(String($(this).attr("data-step")));
+
+        if($(this).hasClass("active") === true) { // 활성화 되어 있다.
+            $(this).removeClass("active");
+            $('#level_' + dataStep).attr('style', 'display:none');
+        } else { // 비활성화 되어 있다.
+            $(this).addClass("active");
+            $('#level_' + dataStep).attr('style', '');
+        }
+
+        levelCheck();
+    });
+
+
 })
+
+
+/* 자동실행 함수 */
 
 // 문항수 그려주기
 function drawItemCounts() {
@@ -608,7 +651,7 @@ function moveToStep0() {
     new_form.attr("name", "new_form");
     new_form.attr("charset", "UTF-8");
     new_form.attr("method", "post");
-    new_form.attr("action", "/customExam/step0");
+    new_form.attr("action", "/exambank/customExam/step0");
 
     //step0 세팅지 리스트를 위한 subjectId
     new_form.append($('<input/>', {type: 'hidden', name: 'subjectId', value: $('#subjectId').val()}));
@@ -616,6 +659,8 @@ function moveToStep0() {
     new_form.appendTo('body');
     new_form.submit();
 }
+
+
 
 let qParam = {};
 
@@ -660,7 +705,7 @@ function moveExamStep2() {
 
     //체크된 단원(최소 단원 - 토픽 or 소주제)
     let chapterArr = [];
-    
+
     console.log("checkedList ::", $('#unit-ul li input[type="checkbox"]:checked'));
     console.log("checkedList ::", $('#unit-ul li input[type="checkbox"]:checked').not('input[type="checkbox"]:checked.depth01'));
 
